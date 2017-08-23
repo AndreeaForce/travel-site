a7b2381349852c0783335e4695b06d25134c6f01
AndreeaForce <andreeaforce@gmail.com>
2017-08-23 18:54:18 +0300

    header

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,11 +3,12 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssimport = require('postcss-import');    
+cssimport = require('postcss-import'),
+hexrgba = require('postcss-hexrgba');
 
 gulp.task('styles', function() {
    return gulp.src('./app/assets/styles/styles.css')
-       .pipe(postcss([cssimport, cssvars, nested, autoprefixer]))
+       .pipe(postcss([cssimport, cssvars, nested, hexrgba, autoprefixer]))
     
      // .on('error', function() {
        //console.log('errorInfo');
